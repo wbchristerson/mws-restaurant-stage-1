80110deb0af08f11880836b57fc193b82a7899b4
Will Christerson <wbchristerson@gmail.com>
2018-03-03 13:03:43 -0500

    refactor: test alt addition to images

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -56,8 +56,9 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
+  image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'A Restaurant';
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
