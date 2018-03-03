2c112ddd3efd06c39985f4a6c496da7a2fdc71c0
Will Christerson <wbchristerson@gmail.com>
2018-03-03 17:02:40 -0500

    refactor: Add alternate text to all content-related images

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = 'Ravens are birds.'
+  image.alt = restaurant.photo_alt;
   li.append(image);
 
   const name = document.createElement('h1');
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,7 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = 'A Restaurant';
+  image.alt = restaurant.photo_alt;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
